3d8f44f3b6ea72274300b46a06fcb60a4f4a7f62
Marko Rusic <markousp5@gmail.com>
2019-06-16 21:23:27 +0200

    Updates

---
+++ precache-manifest.3137f013e155ced1c1af30706e017763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d29d722870ca20c7ad54c08ba96d049",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "b293571d0116d5b6ddc8",
+    "url": "/resume/static/css/main.821f97ba.chunk.css"
+  },
+  {
+    "revision": "990fb2c5570ee66bc883",
+    "url": "/resume/static/js/2.8b3313fa.chunk.js"
+  },
+  {
+    "revision": "b293571d0116d5b6ddc8",
+    "url": "/resume/static/js/main.8d1df26e.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
