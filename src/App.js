95f478701217da20f91aefbe2597aa4f8f2bce8b
Marko Rusic <markousp5@gmail.com>
2019-06-18 18:03:14 +0200

    sort imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import BasicInfo from './components/basic-info'
 import Links from './components/links'
 import WorkExperience from './components/work-experience'
+import Fetch from 'components/fetch'
 import {
   PageContainer,
   FlexContainer,
@@ -14,7 +15,6 @@ import {
   Italic,
   Card
 } from './components/lib'
-import Fetch from 'components/fetch.js'
 
 const App = () => (
   <Fetch url="content.json">
