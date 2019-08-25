417f39514979e2d312b9ba8f271381c5142f80af
Marko Rusic <markousp5@gmail.com>
2019-08-26 00:14:34 +0200

    Updates

---
+++ precache-manifest.82956e76a8abeb4c558954667293547e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1569fbe0f8e4fc7189f617cb7d5759",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "e89f18fcf8cad0efca48",
+    "url": "/resume/static/js/2.a58eb1d4.chunk.js"
+  },
+  {
+    "revision": "a8287bca75e7d0c101f2",
+    "url": "/resume/static/js/main.89597109.chunk.js"
+  },
+  {
+    "revision": "857696afbb2a4f067ea2",
+    "url": "/resume/static/js/runtime~main.cbb32322.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.04f9f10c36c5ec8c5a0cd5750458b61e.js"
+  "/resume/precache-manifest.82956e76a8abeb4c558954667293547e.js"
 );
 
 self.addEventListener('message', (event) => {
