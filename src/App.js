d729f1c36b2e3149889fc4c5dab51b9e706e6d53
Marko Rusic <markousp5@gmail.com>
2019-08-26 00:14:11 +0200

    added ga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 import { createGlobalStyle } from 'styled-components'
 import resume from './data/resume-data.json'
 import ResumeV1 from './resume-v1'
+
+ReactGA.initialize('UA-146442526-1', {
+  debug: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+})
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 const GlobalStyle = createGlobalStyle`
   @import url("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
