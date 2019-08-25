bca08851811eb7b16ba5f32d9994775bcf4b354a
Marko Rusic <markousp5@gmail.com>
2019-08-25 23:40:51 +0200

    fixed links spacing

--- src/resume-v1/components/links.js
+++ src/resume-v1/components/links.js
@@ -3,13 +3,6 @@ import styled from 'styled-components'
 import { chunk } from 'shared/utils'
 import { Flex, Box, Icon, Link, PrintText } from './lib'
 
-const LinkSpacer = styled.div`
-  margin: 12px 0;
-  @media print {
-    margin: 5px 0;
-  }
-`
-
 const LinksContainer = styled(Flex)`
   width: 100%;
   padding: 1em 2em;
@@ -20,20 +13,26 @@ const LinksContainer = styled(Flex)`
   }
 `
 
+const LinkItemContainer = styled.div`
+  margin: 8px 0;
+  @media print {
+    margin: 5px 0;
+  }
+`
+
 const Links = ({ links, itemsPerRow = 3, ...props }) => (
   <LinksContainer {...props}>
     {chunk(links, itemsPerRow).map((_links, i, chunkedLinks) => (
       <React.Fragment key={i}>
         <Box size="5">
           {_links.map(link => (
-            <React.Fragment key={link.id}>
+            <LinkItemContainer key={link.id}>
               <Icon type={link.icon}>
                 <Link href={link.url} target="_blank">
                   <PrintText>{link.text}</PrintText>
                 </Link>
               </Icon>
-              <LinkSpacer />
-            </React.Fragment>
+            </LinkItemContainer>
           ))}
         </Box>
         {i !== chunkedLinks.length - 1 && <Box size="2" />}
