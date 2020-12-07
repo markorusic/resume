0396718cdb153add2ee58a75867efedb16222094
Marko Rusic <markousp5@gmail.com>
2020-12-07 21:33:02 +0100

    fix icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react'
-import { createGlobalStyle } from 'styled-components'
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css'
 import resume from './data/resume-data.json'
 import ResumeV1 from './resume-v1'
 
-const GlobalStyle = createGlobalStyle`
-  @import url("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const App = () => (
-  <>
-    <GlobalStyle />
-    <ResumeV1 resume={resume} />
-  </>
-)
+const App = () => <ResumeV1 resume={resume} />
 
 export default App
