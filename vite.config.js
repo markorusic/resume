c4235d0fc64a91ffe2a72f985329b88329add5f3
Marko Rusic <markousp5@gmail.com>
2021-09-12 15:58:31 +0200

    setup gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: '/resume/',
+  build: {
+    outDir: 'build'
+  },
   server: {
     open: true
   }
